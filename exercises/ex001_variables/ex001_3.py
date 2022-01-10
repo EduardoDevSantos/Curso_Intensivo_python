20caf28da61558da9ecf700ec68eec5d8f1d4a6a
Eduardo Santos <81644656+EduardoDevSantos@users.noreply.github.com>
2022-01-10 15:44:24 -0300

    10/01/2022

--- exercises/ex001_variables/ex001_3.py
+++ exercises/ex001_variables/ex001_3.py
@@ -3,13 +3,14 @@ x = "Awesome"  # Global Variable
 def myfunc():
     y = "fantastic"  # local variable
     print("Python is " + y)
-    # global x
-    # x = "Awesome"
-# Using the global keyword, you can create a global variables inside a function 
-#and can use this in all the code.
+    global z
+    z = "Hard"
+    global x
+    x = "Funny"
+# Using the global keyword, you can create a global variables inside a function and can use this in all the code.
 
 myfunc()
 
 print("Python is " + x) 
-#  print("Python is ", x)
+print("Python is ", z)
 
