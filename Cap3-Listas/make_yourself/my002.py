aa8ba722cded760be2976dd80cc6e1549f7d71e3
Eduardo Santos <81644656+EduardoDevSantos@users.noreply.github.com>
2022-02-16 15:14:13 -0300

    16/02/2022

---
+++ Cap3-Listas/make_yourself/my002.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python3
+names = ['raimundo','manuel','kauan','joel','joão']
+print("Hi "+names[0].title()+" How are you?")
+print("Hi "+names[1].title()+" How are you?")
+print("Hi "+names[2].title()+" How are you?")
+print("Hi "+names[3].title()+" How are you?")
+print("Hi "+names[4].title()+" How are you?")
---
+++ Cap3-Listas/make_yourself/my003.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python3
+cars = ['Ferrari','BMW','Tesla','Bugatti']
+message = "I'll like to buy a " + cars[1]+"."
+print(message)
