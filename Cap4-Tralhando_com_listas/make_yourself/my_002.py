f362dd86dcb1f2c1f09858a6058b53dc9f91f4fb
Eduardo Santos <81644656+EduardoDevSantos@users.noreply.github.com>
2022-02-28 14:04:46 -0300

    28/02/2022

---
+++ Cap4-Tralhando_com_listas/make_yourself/my_002.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+animals = ['Cachorro','Gato','Coelho']
+for animal in animals:
+    print("O "+animal+" é um animal doce e amável.")
+print("Eles são ótimos animais de estimação!!!")
