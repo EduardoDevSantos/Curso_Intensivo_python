83c998957e26a0672a64afd216cc16aa7223bfb8
Eduardo Santos <81644656+EduardoDevSantos@users.noreply.github.com>
2022-04-05 14:36:01 -0300

    05/04/2022

---
+++ exercícios-python/ex009.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python3
+temp_fahr = int(input("Digite a temperatura em Fahrenheit: "))
+temp_cel = int(5*((temp_fahr - 32)/9))
+print("A temperatura em Graus Celcius é igual á: ", temp_cel, "°C")
---
+++ exercícios-python/ex010.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python3
+temp_cel = int(input("Digite a temperatura em Graus °C: "))
+temp_fahr = int(temp_cel * 1.8 + 32)
+print("A temperatura em °F é: ",temp_fahr)
