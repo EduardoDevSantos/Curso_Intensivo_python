8ebd50e14bea45e6676520be7d1e6a7e5f625989
Eduardo Santos <81644656+EduardoDevSantos@users.noreply.github.com>
2022-04-12 16:41:49 -0300

    12/04/2022

---
+++ cap6-Dicionarios/make_yourself/my_001.py
@@ -0,0 +1,12 @@
+people = {
+    'first_name':'eduardo',
+    'last_name':'firmino',
+    'age':17,
+    'city':'iguatu',
+    'future_city':'indaial'
+}
+print("O primeiro nome dessa pessoa é: "+people['first_name'].title()+".")
+print("O Último nome dessa pessoa é: "+people['last_name'].title()+".")
+print("Essa pessoa têm: "+str(people['age'])+" Anos de idade.")
+print("A cidade em que essa pessoa vive é: "+people['city'].title()+".")
+print("A futura cidade dessa pessoa é: "+people['future_city'].title()+".")
