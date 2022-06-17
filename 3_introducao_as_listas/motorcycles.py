84663cdf8b32aaa0b48f1f6ef11d066245b502a5
Eduardo Santos <81644656+EduardoDevSantos@users.noreply.github.com>
2022-06-17 15:07:27 -0300

    17/06/2022

--- 3_introducao_as_listas/motorcycles.py
+++ 3_introducao_as_listas/motorcycles.py
@@ -4,8 +4,23 @@
 # print(motorcycles)
 # motorcycles[0] = 'ducati'
 # print(motorcycles)
+from json import tool
+
+
 motorcycles = []
 motorcycles.append('honda')
 motorcycles.append('yamaha')
 motorcycles.append('suzuki')
 print(motorcycles)
+motorcycles.insert(1,'ducati')
+print(motorcycles)
+del motorcycles[1]
+print(motorcycles)
+last_owned = motorcycles.pop()
+print(motorcycles)
+print("The last motorcycle I owned was a " + last_owned.title() + ".")
+motorcycles = ['honda','yamaha','suzuki','ducati']
+print(motorcycles)
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(print("\nA " + too_expensive.title() + " is too expensive for me."))
---
+++ 3_introducao_as_listas/praticando/changing_the_list.py
@@ -0,0 +1,9 @@
+guests = ['grandfather','mother','father']
+print("Hi " + guests[0].title() + " do you want to go a dinner with me?")
+print("Hi " + guests[1].title() + " do you want to go a dinner with me?")
+print("Hi " + guests[2].title() + " do you want to go a dinner with me?")
+print("Oh, the " +guests[0].title()+ " don't can go to the dinner.")
+guests[0] = 'aunt'
+print("Hi " + guests[0].title() + " do you want to go a dinner with me?")
+print("Hi " + guests[1].title() + " do you want to go a dinner with me?")
+print("Hi " + guests[2].title() + " do you want to go a dinner with me?")
