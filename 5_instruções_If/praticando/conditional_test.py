b438f781a9f692d82353515bae72b5f2fd27f926
Eduardo Santos <81644656+EduardoDevSantos@users.noreply.github.com>
2022-06-22 15:16:18 -0300

    22/06/2022

---
+++ 5_instruções_If/praticando/conditional_test.py
@@ -0,0 +1,80 @@
+car = "bugatti"
+print('car == "bugatti"? I predict True.')
+print(car.lower() == "bugatti")
+print('car == "BMW"? I predict False.')
+print(car == "BMW")
+
+team = "flamengo"
+print('\nteam == "flamengo"? I predict True.')
+print(team == 'flamengo')
+print('team == "santos"? I predict False')
+print(team == "santos")
+
+game = "pes2017"
+print('\ngame == "pes2017"? I predict True.')
+print(game == 'pes2017')
+print('game == "league of legends"? I predict False.')
+print(game == 'league of legends')
+
+name = 'joão'
+print('\nname == "joão"? I predict True.')
+print(name == "joão")
+print('name == "eduardo"? I predict False.')
+print(name == "eduardo")
+
+book = "duna"
+print('\nbook == "duna"? I predict True.')
+print(book == "duna")
+print('book=="harry potter"? I predict False.')
+print(book == "harry potter")
+
+food = 'sandwich'
+print("\nfood == 'sandwich'?")
+print(food == 'sandwich')
+print("food == 'Sandwich'?")
+print(food == 'Sandwich')
+print("\nfood != 'pizza'?")
+print(food != 'pizza')
+print("food != 'sandwich'?")
+print(food != 'sandwich')
+
+music = 'Mirrors'
+print("\nmusic.lower() == 'mirrors'?")
+print(music.lower() == 'mirrors')
+print("music.lower() == 'someone you loved'?")
+print(music.lower() == 'someone you loved?')
+# Numeros
+print("\n10 == 7 ?")
+print(10 == 7)
+print("10 == 10 ?")
+print(10 == 10)
+print("10 != 7 ?")
+print(10 != 7)
+print("10 != 10 ?")
+print(10 != 10)
+print("10 > 5 ?")
+print(10 > 5)
+print("10 >= 12 ?")
+print(10 >= 12)
+print("7 < 10 ?")
+print(7 < 10)
+print("7 <= 5 ?")
+print(7 <= 5)
+
+age_1 = 12
+age_2 = 18
+print("\n")
+print(age_1 >= 18 and age_2 >= 18)
+age_1 = 30
+print(age_1 >= 18 and age_2 >= 18)
+print(age_1 <= 18 and age_2 <= 18)
+age_1 = 10
+age_2 = 8
+print(age_1 <= 18 and age_2 <= 18)
+
+users = ['eduardo','joão','lucas','maria']
+print("\n")
+print('eduardo' in users)
+print('ana' in users)
+print('ana' not in users)
+print('lucas' not in users)
